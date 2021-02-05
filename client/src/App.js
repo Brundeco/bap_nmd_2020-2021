6499d7178787f7869d17171897c60b97326298e6
gdm-1718-brundeco <32094744+gdm-1718-brundeco@users.noreply.github.com>
2021-02-05 10:42:49 +0100

    Setup server - Connection with MongoDB

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import "./App.scss";
 import "./components/Nav";
 import "./pages/Home";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./pages";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path='/' component={Home} />
+        <Route exact path="/" component={Home} />
       </div>
     </Router>
   );
---
+++ server/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = `mongodb+srv://Bruno:${process.env.MONGODB_PASSWORD}@cluster0.2gkzu.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
