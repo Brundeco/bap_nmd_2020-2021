26710b437f7cf4c933c1defe7f2e47ecd0235b4d
gdm-1718-brundeco <32094744+gdm-1718-brundeco@users.noreply.github.com>
2021-03-03 11:29:10 +0100

    Pass form data from child to parent

--- client/src/pages/Properties/CreateProperty.js
+++ client/src/pages/Properties/CreateProperty.js
@@ -1,35 +1,20 @@
 import React, { useState, useEffect } from "react";
-import {
-  InputField,
-  CheckSession,
-  Textarea,
-  PrevPage,
-} from "./../../components";
-import SelectImage from "./../../icons/selectimage.svg";
-
-import FileBase from "react-file-base64";
+import { CheckSession, PrevPage } from "./../../components";
 import axios from "axios";
 import { PropertyForm } from "..";
 
-export default () => {
+export default (props) => {
   CheckSession(localStorage.getItem("jwt"));
 
-  const user = JSON.parse(localStorage.getItem("user"));
-  const [data, setData] = React.useState({
-    author: user.username,
-    author_id: user.id,
-  });
-  const [images, setImages] = useState([]);
-
-  const handleChange = (name, value) => {
-    setData((prev) => ({ ...prev, [name]: value }));
+  const [data, setData] = React.useState();
+  
+  const handleData = (formData) => {
+    setData(formData);
   };
 
-  const handleSubmit = (data) => {
-    // e.preventDefault();
-    console.log(data)
-    // console.log("handle submot triggered");
-    // console.log(images);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
     // axios.post("http://localhost:5000/properties", {
     //   title: data.title,
     //   author_id: data.author_id,
@@ -42,8 +27,7 @@ export default () => {
     <div className="create-product-screen">
       <div className="page-wrapper">
         <PrevPage />
-        <h1>Fill out the data below to start hosting your property</h1>
-        <PropertyForm onSubmit={(data) => handleSubmit(data) } />
+        <PropertyForm onSubmit={handleSubmit} formdata={handleData} />
       </div>
     </div>
   );
