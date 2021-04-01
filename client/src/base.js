bd252604a8b79a4d9225018e3c710c2bb0064f0b
gdm-1718-brundeco <brundeco@student.arteveldehs.be>
2021-04-01 22:20:00 +0200

    Fix - Branch switch mistake

--- client/src/base.js
+++ client/src/base.js
@@ -4,7 +4,7 @@ import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "bachelorproef-2020-2021.firebaseapp.com",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL:
     "https://bachelorproef-2020-2021-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "bachelorproef-2020-2021",
