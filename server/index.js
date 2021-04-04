628db1624673ed1c4c0c34d9f10cefbd6b95d1a5
gdm-1718-brundeco <brundeco@student.arteveldehs.be>
2021-04-04 17:42:52 +0200

    Update - Deployment

--- server/index.js
+++ server/index.js
@@ -14,23 +14,40 @@ import userRoutes from './routes/users.js'
 import chatRoutes from './routes/chat.js'
 import reservationRoutes from './routes/reservations.js'
 
-app.use(cors())
-dotenv.config()
-
 const app = express()
+const router = express.Router()
 const server = http.createServer(app)
 const socketio = new io.Server(server)
+
+// const socketio = new io.Server(server, {
+//   handlePreflightRequest: (req, res) => {
+//     const headers = {
+//       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+//       'Access-Control-Allow-Origin': req.headers.origin, //or the specific origin you want to give access to,
+//       'Access-Control-Allow-Credentials': true,
+//     }
+//     res.writeHead(200, headers)
+//     res.end()
+//   },
+// })
 
 app.get('/', (req, res) => {
   res.send('index.js was restructured')
 })
 
+router.get('/rooms/:roomId/users')
+
+dotenv.config()
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
 
 let userList = []
 
 socketio.on('connection', (socket) => {
+  // console.log(socket)
+
   socket.on('registration', (id) => {
     userList.push({ sid: socket.id, id: id })
     console.log(userList)
