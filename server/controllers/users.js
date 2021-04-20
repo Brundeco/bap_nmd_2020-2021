6cc5dfb1fef8af2c2a05979ec7e25b8c15dd36a5
Bruno De Coene <brundeco@student.arteveldehs.be>
2021-04-21 00:30:39 +0200

    Update - Deployment

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -99,7 +99,7 @@ export const passwordReset = async (req, res) => {
 
       const transporter = nodemailer.createTransport({
         host: 'smtp-mail.outlook.com', // hostname
-        secureConnection: true, // TLS requires secureConnection to be false
+        secureConnection: false, // TLS requires secureConnection to be false
         port: 587, // port for secure SMTP
         tls: {
           ciphers: 'SSLv3',
